6106bd96c8572ae9dc51eed17256c74bf5a0f68f
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2025-09-10 20:48:19 +0700

    Create float_ops.h

---
+++ float_ops.h
@@ -0,0 +1,9 @@
+#ifndef FLOAT_OPS_H
+#define FLOAT_OPS_H
+
+float float_add(float a, float b);
+float float_subtract(float a, float b);
+float float_multiply(float a, float b);
+float float_divide(float a, float b);
+
+#endif
